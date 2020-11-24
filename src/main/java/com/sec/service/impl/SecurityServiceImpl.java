1c08b13d8aa4f7a034ea7ba4da380612b1e80946
LastChance89 <ksmitw@gmail.com>
2020-11-23 22:57:45 -0500

    changes so security works properly

--- src/main/java/com/sec/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/sec/service/impl/SecurityServiceImpl.java
@@ -2,12 +2,17 @@ package com.sec.service.impl;
 
 import java.util.List;
 
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.sec.dao.UserDao;
 import com.sec.service.SecurityService;
-
+@Service
 public class SecurityServiceImpl implements SecurityService {
 
 	@Autowired
